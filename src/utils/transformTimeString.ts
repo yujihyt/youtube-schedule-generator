7ec9d95cdb53aec7a37f5268e9b87ea5eb1b2c8d
yujihyt <yuji_teixeira@hotmail.com>
2023-02-14 14:03:51 -0300

    fix transform time string regex

--- src/utils/transformTimeString.ts
+++ src/utils/transformTimeString.ts
@@ -1,6 +1,6 @@
 export const transformTimeString = (time) => {
   const [, hour = '0', minute = '0', second = '0'] = time.match(
-    /(\d+)?H(\d+M)?(\d+)?S/,
+    /PT(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?/,
   );
   return {
     hour: parseInt(hour),
